d8e38930c7e673160624efb46e50b5e7f168c9f6
Carlos Andrés Salazar Melita <andres.melita.dev@gmail.com>
2025-11-30 04:52:23 -0300

    feat: new methods added 💼

--- internal/domain/ports/payment_infra_service.go
+++ internal/domain/ports/payment_infra_service.go
@@ -8,7 +8,9 @@ import (
 // PaymentInfraService define el caso de uso para operaciones de infraestructura de pagos
 type PaymentInfraService interface {
 	GetPaymentInfraByQrValue(ctx context.Context, qrValue string) (*model.PaymentInfra, error)
-	GetAvailableLockers(ctx context.Context, paymentRackID int, bookingTimeID int) (*model.AvailableLockers, error)
-	ValidateDiscountCoupon(ctx context.Context, couponCode string) (*model.DiscountCouponValidation, error)
-	GeneratePurchaseOrder(ctx context.Context, groupID int, couponCode *string, userEmail string, userPhone string) (*model.PurchaseOrder, error)
+	GetAvailableLockers(ctx context.Context, paymentRackID int, bookingTimeID int, traceID string) (*model.AvailableLockers, error)
+	ValidateDiscountCoupon(ctx context.Context, couponCode string, rackID int, traceID string) (*model.DiscountCouponValidation, error)
+	GeneratePurchaseOrder(ctx context.Context, groupID int, couponCode *string, userEmail string, userPhone string, traceID string, gatewayName string) (*model.PurchaseOrder, error)
+	GenerateBooking(ctx context.Context, purchaseOrder string, traceID string) (*model.Booking, error)
+	GetPurchaseOrderByPo(ctx context.Context, purchaseOrder string, traceID string) (*model.PurchaseOrderData, error)
 }
