0b2542c5f31b8f5f5b111638f87a5f68b9ce792f
Carlos Andrés Salazar Melita <andres.melita.dev@gmail.com>
2025-11-30 00:00:10 -0300

    fix: Input GetPaymentInfraByIDInput renamed 🖋️

--- internal/domain/ports/payment_infra_service.go
+++ internal/domain/ports/payment_infra_service.go
@@ -7,7 +7,7 @@ import (
 
 // PaymentInfraService define el caso de uso para operaciones de infraestructura de pagos
 type PaymentInfraService interface {
-	GetPaymentInfraByID(ctx context.Context, paymentRackID string) (*model.PaymentInfra, error)
+	GetPaymentInfraByQrValue(ctx context.Context, qrValue string) (*model.PaymentInfra, error)
 	GetAvailableLockers(ctx context.Context, paymentRackID int, bookingTimeID int) (*model.AvailableLockers, error)
 	ValidateDiscountCoupon(ctx context.Context, couponCode string) (*model.DiscountCouponValidation, error)
 	GeneratePurchaseOrder(ctx context.Context, groupID int, couponCode *string, userEmail string, userPhone string) (*model.PurchaseOrder, error)
